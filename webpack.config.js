ae4846939be0826c3dfa192eca838ffabf34b002
TheCosmoNomad <anubhav.das@protonmail.com>
2025-02-28 00:30:13 +0530

    Added portfinder

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
 const path = require("path");
+const portfinder = require("portfinder");
 
-module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "src"),
+module.exports = async () => {
+  const port = await portfinder.getPortPromise({ port: 3000 });
+
+  return {
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: "./src/index.ts",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
     },
-    hot: true,
-    open: true,
-    port: 3000,
-  },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "src"),
+      },
+      hot: true,
+      open: true,
+      port,
+    },
+  };
 };
